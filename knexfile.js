c529ac984d4780c6665d0d03c67ec6bd066cc85b
Roberto  Varela <rvarela11@Roberts-MacBook-Pro.local>
2016-10-12 11:56:05 -0500

    fixed connection

--- knexfile.js
+++ knexfile.js
@@ -20,9 +20,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: {
-      database: 'postgres://maahslambruzur:_23eKARkyt2f2yPQRNQEyBgaIf@ec2-50-17-237-148.compute-1.amazonaws.com:5432/d6gtkf7k4ag2qm'
-    }
+    connection: 'postgres://maahslambruzur:_23eKARkyt2f2yPQRNQEyBgaIf@ec2-50-17-237-148.compute-1.amazonaws.com:5432/d6gtkf7k4ag2qm'
   }
 
 
